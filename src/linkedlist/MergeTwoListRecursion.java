920c43638aced2a8824d32eab6e9999060af40e6
JulyPi <dovanphu876@gmail.com>
2025-10-06 22:35:57 +0700

    Update MergeTwoListRecursion.java

--- src/linkedlist/MergeTwoListRecursion.java
+++ src/linkedlist/MergeTwoListRecursion.java
@@ -1,5 +1,21 @@
 package linkedlist;
 
 public class MergeTwoListRecursion {
+  class Node {
+        int data;
+        Node next;
+    }
 
+    public Node mergeTwoLists(Node head1, Node head2) {
+        if (head1 == null) return head2;
+        if (head2 == null) return head1;
+
+        if (head1.data < head2.data) {
+            head1.next = mergeTwoLists(head1.next, head2);
+            return head1;
+        } else {
+            head2.next = mergeTwoLists(head1, head2.next);
+            return head2;
+        }
+    }
 }
