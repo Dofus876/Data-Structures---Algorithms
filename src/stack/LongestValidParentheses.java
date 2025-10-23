8021c00b4295b5e39d944e5144bef880ec2f0871
Do Phu <dovanphu876@gmail.com>
2025-10-23 23:04:00 +0700

    Done longestValidParentheses

--- src/stack/LongestValidParentheses.java
+++ src/stack/LongestValidParentheses.java
@@ -1,6 +1,6 @@
 package stack;
 
-// import java.util.Stack;
+import java.util.Stack;
 
 /**
  * Hàm tìm độ dài của chuỗi con ngoặc đúng dài nhất trong chuỗi đầu vào.
@@ -9,8 +9,30 @@ package stack;
  * @return Độ dài lớn nhất của một đoạn ngoặc đúng liên tiếp trong chuỗi.
  */
 public class LongestValidParentheses {
-    public int longestValidParentheses(String s) {
-        // TODO: Implement using Stack hướng dẫn đã gợi ý
-        return 0;
+    public static int longestValidParentheses(String s) {
+        int maxLen = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1); // Mốc cơ sở ban đầu
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(i);
+            } else { // c == ')'
+                stack.pop();
+                if (stack.isEmpty()) {
+                    // Dấu ')' này không hợp lệ, nó trở thành mốc cơ sở mới
+                    stack.push(i);
+                } else {
+                    // Tính độ dài từ chỉ số hiện tại đến mốc trên đỉnh stack
+                    maxLen = Math.max(maxLen, i - stack.peek());
+                }
+            }
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestValidParentheses("()(()((((())"));
     }
 }
